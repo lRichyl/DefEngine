f5f0ec29838ebfe3df0ed156b28f1f39fc30ac35
Luis Núñez <panicjoker03@outlook.com>
2023-04-06 11:44:58 -0600

    Typedef aliasing and more maths!!!
    More functions added to the math module of def_engine. It now supports abs, sign, 2d vector magnitude, and normalizing 2d vector.

--- def_math.cpp
+++ def_math.cpp
@@ -1,8 +1,8 @@
 #include "def_math.h"
-
+#include "math.h"
 namespace def {
      // Mirror x.
-     void V2::SwitchXComponents(V2 *a, V2 *b){
+     void VEC_2D::SwitchXComponents(VEC_2D *a, VEC_2D *b){
           float temporalX = a->x;
 
           a->x = b->x;
@@ -10,10 +10,52 @@ namespace def {
      }
 
      // Mirror y.
-     void V2::SwitchYComponents(V2 *a, V2 *b){
+     void VEC_2D::SwitchYComponents(VEC_2D *a, VEC_2D *b){
           float temporalY = a->y;
 
           a->y = b->y;
           b->y = temporalY;
+     }
+
+
+     float VEC_2D::Magnitude(const VEC_2D &val)
+     {
+    	 return sqrtf(val.x * val.x + val.y * val.y);
+     }
+     //ABS FUNC
+     //Could be Calculated as abs(x) = sqrt(x * x)...
+     //but is calculated as case statement:
+     //|x| = {  x  if x ≥ 0  }
+     //|x| = { -x  if x < 0  }
+     //with special case when x is equal to zero, then return zero
+     float abs(float val) noexcept
+     {
+    	 if(val < F_ZERO_APPROXIMATION && val > F_ZERO_APPROXIMATION)
+			 return 0.0f;
+    	 if(val >= F_ZERO_APPROXIMATION)
+    		 return val;
+    	 else
+    		 return - val;
+     }
+
+     //SIGN FUN
+     //Calculated as sign(x) = x / abs(x)
+     //with special case when x is equal to zero, then return zero
+     float sign(float val) noexcept
+     {
+    	 if(val < F_ZERO_APPROXIMATION && val > F_ZERO_APPROXIMATION)
+    		 return 0.0f;
+    	 return val / abs(val);
+     }
+     //divide each component by the vector magnitude
+     VEC_2D VEC_2D::Normalized(const VEC_2D &val)
+     {
+    	 VEC_2D _out{0.0f, 0.0f};
+    	 float magnitude = Magnitude(val);
+    	 if(!abs(magnitude) > 0.0f)
+    		 return _out;
+    	 _out.x = val.x / magnitude;
+    	 _out.y = val.y / magnitude;
+    	 return _out;
      }
 }
--- include/def_math.h
+++ include/def_math.h
@@ -1,41 +1,49 @@
 #ifndef DEF_MATH_H
 #define DEF_MATH_H
+#define F_ZERO_APPROXIMATION 0.00000001f
+#define F_INVERSE_SQRT_2 0.70710678118f
 
 namespace def {
-     struct V2{
+     typedef struct def_f_vec_2{
           float x;
           float y;
-          V2() :x(0), y(0) {}
-          V2(float x, float y) : x(x), y(y) {}
-          static void SwitchXComponents(V2 *a, V2 *b);
-          static void SwitchYComponents(V2 *a, V2 *b);
-     };
+          def_f_vec_2() :x(0), y(0) {}
+          def_f_vec_2(float x, float y) : x(x), y(y) {}
+          static void SwitchXComponents(def_f_vec_2 *a, def_f_vec_2 *b);
+          static void SwitchYComponents(def_f_vec_2 *a, def_f_vec_2 *b);
+          static def_f_vec_2 Normalized(const def_f_vec_2 &val);
+          static float Magnitude(const def_f_vec_2 &val);
+          //todo:: operator overload... check vectorian library...
+     }VEC_2D;
 
-     struct V3{
+     typedef struct def_f_vec_3{
           float x;
           float y;
           float z;
-          V3() :x(0), y(0), z(0) {}
-          V3(float x, float y, float z) : x(x), y(y), z(z) {}
-     };
+          def_f_vec_3() :x(0), y(0), z(0) {}
+          def_f_vec_3(float x, float y, float z) : x(x), y(y), z(z) {}
+     }VEC_3D;
 
-     struct V4{
+     typedef struct def_f_vec_4{
           float x = 0;
           float y = 0;
           float z = 0;
      	 float w = 0;
-          V4() :x(0), y(0), z(0), w(0) {}
-          V4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
-     };
+          def_f_vec_4() :x(0), y(0), z(0), w(0) {}
+          def_f_vec_4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+     }VEC_4D;
 
-     struct Rect{
+     typedef struct def_f_rect{
      	float x;
      	float y;
      	float w;
      	float h;
-         Rect() :x(0), y(0), w(0), h(0) {}
-         Rect(float x, float y, float w, float h) : x(x), y(y), w(w), h(h) {}
-     };
+         def_f_rect() :x(0), y(0), w(0), h(0) {}
+         def_f_rect(float x, float y, float w, float h) : x(x), y(y), w(w), h(h) {}
+     }RECT;
+
+     float abs(float val) noexcept;
+     float sign(float val) noexcept;
 }
 
 #endif
